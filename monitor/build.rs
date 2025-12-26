e2f86eace838874b75212f1f89154b1ea7775890
Noah Hood <hood.noah@icloud.com>
2025-12-26 18:18:30 -0500

    working mqtt

--- monitor/build.rs
+++ monitor/build.rs
@@ -13,6 +13,18 @@ fn main() {
         println!("cargo:rustc-env=WIFI_PASSWORD={}", wifi_pass);
     }
 
+    if let Ok(mqtt_endpoint) = env::var("MQTT_ENDPOINT") {
+        println!("cargo:rustc-env=MQTT_ENDPOINT={}", mqtt_endpoint);
+    }
+
+    if let Ok(mqtt_user) = env::var("MQTT_USER") {
+        println!("cargo:rustc-env=MQTT_USER={}", mqtt_user);
+    }
+
+    if let Ok(mqtt_pass) = env::var("MQTT_PASS") {
+        println!("cargo:rustc-env=MQTT_PASS={}", mqtt_pass);
+    }
+
     println!("cargo:rerun-if-changed=.env");
 
     // embuild stuff
--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -10,6 +10,7 @@ use esp_idf_svc::{
         },
         units::*,
     },
+    mqtt::client::*,
     nvs::EspDefaultNvsPartition,
     wifi::{BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
@@ -18,6 +19,9 @@ use woodstove_lib::Temperature;
 
 const WIFI_SSID: &str = env!("WIFI_SSID");
 const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
+const MQTT_ENDPOINT: &str = env!("MQTT_ENDPOINT");
+const MQTT_USER: &str = env!("MQTT_USER");
+const MQTT_PASS: &str = env!("MQTT_PASS");
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -29,6 +33,9 @@ fn main() -> anyhow::Result<()> {
 
     // setup SPI for thermocouple reads
     let peripherals = Peripherals::take().unwrap();
+
+    // setup error led
+    let mut status_led = PinDriver::output(peripherals.pins.gpio8)?;
 
     let freq: Hertz = 4.MHz().into();
     let mode = Mode {
@@ -42,27 +49,20 @@ fn main() -> anyhow::Result<()> {
 
     let mut spi = SpiDeviceDriver::new_single(
         peripherals.spi2,
-        peripherals.pins.gpio48,       // CLK
-        peripherals.pins.gpio38,       // DO (dummy, just put one randomly)
-        Some(peripherals.pins.gpio47), // sensor -> MCU
+        peripherals.pins.gpio48,       // CLK (D13/GPIO48)
+        peripherals.pins.gpio38,       // (dummy, just put one randomly)
+        Some(peripherals.pins.gpio47), // DO (D12/GPIO47)
         Option::<AnyIOPin>::None,      // handled elsewhere, CS
         &bus_config,
         &config,
     )
     .unwrap();
 
-    let mut cs = PinDriver::output(peripherals.pins.gpio21).unwrap();
+    let mut cs = PinDriver::output(peripherals.pins.gpio21).unwrap(); // CS (D10/GPIO21)
 
     log::info!("SPI and CS configured successfully!");
 
     // Setup wifi
-    log::info!("SSID: '{}'", WIFI_SSID);
-    log::info!(
-        "Password: '{}' (len: {})",
-        WIFI_PASSWORD,
-        WIFI_PASSWORD.len()
-    );
-
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
@@ -82,21 +82,56 @@ fn main() -> anyhow::Result<()> {
     wifi.connect()?;
     log::info!("wifi connected");
 
+    let mqtt_config = MqttClientConfiguration {
+        client_id: Some("woodstove_monitor"),
+        username: Some(MQTT_USER),
+        password: Some(MQTT_PASS),
+        ..Default::default()
+    };
+
+    let (mut mqtt_client, _) = EspMqttClient::new(MQTT_ENDPOINT, &mqtt_config)?;
+
     loop {
         match Max31855::read_thermocouple(&mut spi, &mut cs, Unit::Celsius) {
             Ok(temp_c) => {
                 let temp = Temperature::from_celsius(temp_c);
-                log::info!(
-                    "Temperature: {:.1}°F ({:.1}°C)",
-                    temp.fahrenheit(),
-                    temp.celsius()
-                );
+
+                // publish temperature
+                mqtt_client
+                    .publish(
+                        "woodstove/temperature",
+                        QoS::AtMostOnce,
+                        false,
+                        temp.fahrenheit().to_string().as_bytes(),
+                    )
+                    .ok();
+
+                // publish status
+                mqtt_client
+                    .publish("woodstove/status", QoS::AtMostOnce, true, b"online")
+                    .ok();
+
+                status_led.set_low().ok();
+
+                log::info!("Published: {:.1}F", temp.fahrenheit());
             }
             Err(e) => {
+                let error_msg = format!("Sensor error: {:?}", e);
+                mqtt_client
+                    .publish(
+                        "woodstove/error",
+                        QoS::AtLeastOnce,
+                        false,
+                        error_msg.as_bytes(),
+                    )
+                    .ok();
+
+                status_led.set_high().ok();
+
                 log::error!("Sensor error: {:?}", e);
             }
         }
 
-        FreeRtos::delay_ms(2000);
+        FreeRtos::delay_ms(10000);
     }
 }
