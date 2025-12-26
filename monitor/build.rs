4fa4eda417a4546d36785e89b10dd94a45140bc5
Noah Hood <hood.noah@icloud.com>
2025-12-26 12:45:19 -0500

    setup wifi, switch to arduino nano esp32 since wifi is broken on esp32c3
    checkpoint before switching to ardunio esp32
    
    setup wifi, switch to arduino nano esp32 since wifi is broken on esp32c3

--- monitor/build.rs
+++ monitor/build.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
+    // load .env file for build
+    dotenvy::dotenv().ok();
+
+    // re-export as cargo env vars
+    if let Ok(ssid) = env::var("WIFI_SSID") {
+        println!("cargo:rustc-env=WIFI_SSID={}", ssid);
+    }
+
+    if let Ok(wifi_pass) = env::var("WIFI_PASSWORD") {
+        println!("cargo:rustc-env=WIFI_PASSWORD={}", wifi_pass);
+    }
+
+    println!("cargo:rerun-if-changed=.env");
+
+    // embuild stuff
     embuild::espidf::sysenv::output();
 }
--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,17 +1,25 @@
-use esp_idf_svc::hal::{
-    delay::FreeRtos,
-    gpio::*,
-    peripherals::Peripherals,
-    spi::{
-        config::{Config, DriverConfig, Mode, Phase},
-        *,
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{
+        delay::FreeRtos,
+        gpio::*,
+        peripherals::Peripherals,
+        spi::{
+            config::{Config, DriverConfig, Mode, Phase},
+            *,
+        },
+        units::*,
     },
-    units::*,
+    nvs::EspDefaultNvsPartition,
+    wifi::{BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use max31855::{Max31855, Unit};
 use woodstove_lib::Temperature;
 
-fn main() {
+const WIFI_SSID: &str = env!("WIFI_SSID");
+const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -19,8 +27,7 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Beginning test of temperature measurement...");
-
+    // setup SPI for thermocouple reads
     let peripherals = Peripherals::take().unwrap();
 
     let freq: Hertz = 4.MHz().into();
@@ -35,18 +42,45 @@ fn main() {
 
     let mut spi = SpiDeviceDriver::new_single(
         peripherals.spi2,
-        peripherals.pins.gpio6,
-        peripherals.pins.gpio10,
-        Some(peripherals.pins.gpio2),
-        Option::<AnyIOPin>::None,
+        peripherals.pins.gpio48,       // CLK
+        peripherals.pins.gpio38,       // DO (dummy, just put one randomly)
+        Some(peripherals.pins.gpio47), // sensor -> MCU
+        Option::<AnyIOPin>::None,      // handled elsewhere, CS
         &bus_config,
         &config,
     )
     .unwrap();
 
-    let mut cs = PinDriver::output(peripherals.pins.gpio7).unwrap();
+    let mut cs = PinDriver::output(peripherals.pins.gpio21).unwrap();
 
     log::info!("SPI and CS configured successfully!");
+
+    // Setup wifi
+    log::info!("SSID: '{}'", WIFI_SSID);
+    log::info!(
+        "Password: '{}' (len: {})",
+        WIFI_PASSWORD,
+        WIFI_PASSWORD.len()
+    );
+
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop.clone(),
+    )?;
+
+    wifi.start()?;
+
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: WIFI_SSID.try_into().unwrap(),
+        password: WIFI_PASSWORD.try_into().unwrap(),
+        ..Default::default()
+    }))?;
+
+    wifi.connect()?;
+    log::info!("wifi connected");
 
     loop {
         match Max31855::read_thermocouple(&mut spi, &mut cs, Unit::Celsius) {
